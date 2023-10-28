2ccc8e0becc7186f699db4b9f490acf93d4f5f1f
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-10-27 22:18:35 -0300

    initial commit

---
+++ SnowSeeke/SnowSeeke/SnowSeekeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekeApp.swift
+//  SnowSeeke
+//
+//  Created by Isaque da Silva on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
